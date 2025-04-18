a5fdc5b01f1571b592361f0c0684c8a36ca2a45f
Luiz54jr <lcrodrigues15@hotmail.com>
2025-04-17 21:33:53 -0300

    Update CurrencyService.java

--- src/CurrencyService.java
+++ src/CurrencyService.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class CurrencyService {
+    private ExchangeRateApi api = new ExchangeRateApi();
+
+    public double convert(String from, String to, double amount) {
+        double rate = api.getExchangeRate(from, to);
+        return amount * rate;
+    }
 }
+
